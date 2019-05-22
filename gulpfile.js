0504f8372fff0c05619729a837d348ed4958f92d
Grzegorz Domaradzki <gd@tcz.pl>
2019-05-22 14:32:25 +0200

    initial commit with package.json and added gulpfile with configuration of browsersync and css/sccs file"

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+var browserSync = require("browser-sync").create();
+	
+var paths = {
+    styles: {
+        src: "src/scss/*.scss",
+        dest: "src/css"
+    }
+
+};
+
+function style() {
+    return (
+        gulp
+            .src(paths.styles.src)
+            .pipe(sourcemaps.init())
+            .pipe(sass())
+            .on("error", sass.logError)
+            .pipe(postcss([autoprefixer(), cssnano()]))
+            .pipe(sourcemaps.write())
+            .pipe(gulp.dest(paths.styles.dest))
+            .pipe(browserSync.stream())
+    );
+}
+exports.style = style;
+
+function reload(done) {
+    browserSync.reload();
+    done();
+}
+	
+function watch(){
+    browserSync.init({
+        server: {
+            baseDir: "./src"
+        }
+    });
+    gulp.watch(paths.styles.src, style)
+    gulp.watch("src/*.html", reload); 
+}
+
+exports.default = watch;
